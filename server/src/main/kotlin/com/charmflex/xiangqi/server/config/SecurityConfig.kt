56e502739d25a9626a37b50953551a2bd708583a
charmflex <charmflex1111@gmail.com>
2026-02-28 15:36:12 +0800

    update download link

--- server/src/main/kotlin/com/charmflex/xiangqi/server/config/SecurityConfig.kt
+++ server/src/main/kotlin/com/charmflex/xiangqi/server/config/SecurityConfig.kt
@@ -26,7 +26,7 @@ internal class SecurityConfig(
             .csrf { obj: CsrfConfigurer<HttpSecurity> -> obj.disable() }
             .authorizeHttpRequests { auth ->
                 auth
-                    .requestMatchers("/api/auth/**", "/ws", "/ws/**", "/intro/**", "/screenshots/**")
+                    .requestMatchers("/api/auth/**", "/ws", "/ws/**", "/intro/**", "/downloads/**", "/screenshots/**")
                     .permitAll()
                     .anyRequest()
                     .authenticated()
