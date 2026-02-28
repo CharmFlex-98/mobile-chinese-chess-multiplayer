3fe25ede0955b43b5bcaa036347febc79ac16636
charmflex <charmflex1111@gmail.com>
2026-02-28 12:05:21 +0800

    update url

---
+++ server/src/main/kotlin/com/charmflex/xiangqi/server/controller/WebController.kt
@@ -0,0 +1,10 @@
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class WebController {
+    @GetMapping("/intro")
+    fun intro(): String {
+        return "forward:/intro/index.html"
+    }
+}
